c7a39ecdfd48bdbde98864e217cfbead5961c0b6
rmoise <roderickfmoise@gmail.com>
2022-11-16 23:06:34 +0100

    add margin-top to search input field

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -8,7 +8,7 @@ const VisibilityInputFilter = ({visibilityFilter, dispatch}) => {
     return (
       <Container>
         <Form.Control
-          className="mb-2"
+          className="mb-2 mt-3"
           onChange={(e) => dispatch(setFilter(e.target.value))}
           value={visibilityFilter}
           placeholder="Serach by movie title"
