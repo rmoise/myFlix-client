70e43bd17c9b35bfad0c3bf8f1f601232bd4a71c
rmoise <roderickfmoise@gmail.com>
2022-11-17 16:25:59 +0100

    update spacing on search input  field

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -8,7 +8,7 @@ const VisibilityInputFilter = ({visibilityFilter, dispatch}) => {
     return (
       <Container>
         <Form.Control
-          className="mb-2 mt-3"
+          className="mb-1 mt-2"
           onChange={(e) => dispatch(setFilter(e.target.value))}
           value={visibilityFilter}
           placeholder="Serach by movie title"
